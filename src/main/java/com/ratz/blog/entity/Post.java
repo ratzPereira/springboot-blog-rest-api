f758646144ce98a79b68da277646ae7a7fc7902e
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-01-19 11:55:20 -0100

    created database connection also created post entity

---
+++ src/main/java/com/ratz/blog/entity/Post.java
@@ -0,0 +1,32 @@
+package com.ratz.blog.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})})
+public class Post {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String title;
+
+  @Column(nullable = false)
+  private String description;
+
+  @Column(nullable = false)
+  private String content;
+
+}
