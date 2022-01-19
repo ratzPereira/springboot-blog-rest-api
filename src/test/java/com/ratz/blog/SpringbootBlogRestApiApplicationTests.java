f345f133da6c1a6025f357ff5adc6fe6fe7dad4e
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-01-19 09:59:12 -0100

    Initial commit

---
+++ src/test/java/com/ratz/blog/SpringbootBlogRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratz.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBlogRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
