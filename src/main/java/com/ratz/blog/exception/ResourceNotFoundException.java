a63718cd71f3754c69a308c6f5660c31df303c23
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-01-19 16:32:32 -0100

    created custom exception for resource not found

---
+++ src/main/java/com/ratz/blog/exception/ResourceNotFoundException.java
@@ -0,0 +1,24 @@
+package com.ratz.blog.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Getter
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+  private String resourceName;
+  private String fieldName;
+  private String fieldValue;
+
+
+  public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue) {
+
+    super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+
+    this.fieldName = fieldName;
+    this.fieldValue = fieldValue;
+    this.resourceName = resourceName;
+  }
+}
