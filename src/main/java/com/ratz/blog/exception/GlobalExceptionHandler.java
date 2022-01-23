c22a31569bf0815fd7fdbf1db3e5462b1e3de21a
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-01-22 23:41:53 -0100

    created global exception handler costumized response

--- src/main/java/com/ratz/blog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ratz/blog/exception/GlobalExceptionHandler.java
@@ -21,5 +21,24 @@ public class GlobalExceptionHandler {
     return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
   }
 
+  @ExceptionHandler(BlogAPIException.class)
+  public ResponseEntity<ErrorDetails> handleBlogAPIException(BlogAPIException exception , WebRequest webRequest){
+
+    ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(),webRequest.getDescription(false));
+
+    return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+  }
+
+
+
+
   //handle global exception
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception , WebRequest webRequest){
+
+    ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(),webRequest.getDescription(false));
+
+    return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
 }
