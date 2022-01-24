b915dc4ef435290e9d6094aaf37455ee89e47490
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-01-24 15:21:54 -0100

    created User and Role entities and their relationship

---
+++ src/main/java/com/ratz/blog/entity/Role.java
@@ -0,0 +1,19 @@
+package com.ratz.blog.entity;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "roles" )
+public class Role {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(length = 60)
+  private String name;
+}
