5458c26bd502ce1841baf344442332e03431da74
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-01-26 11:13:59 -0100

    Customized Swagger models documentation with anotations. Finished the documentation and the projet. deployment in AWS comming next

--- src/main/java/com/ratz/blog/DTO/CommentDTO.java
+++ src/main/java/com/ratz/blog/DTO/CommentDTO.java
@@ -1,5 +1,7 @@
 package com.ratz.blog.DTO;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
@@ -7,19 +9,24 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 
+@ApiModel(value = "Comment model information")
 @Data
 public class CommentDTO {
 
+  @ApiModelProperty(value = "Blog Comment ID")
   private Long id;
 
+  @ApiModelProperty(value = "Blog Comment name")
   @NotEmpty
   @Size(min = 2, message = "Title must have 2 characters")
   private String name;
 
+  @ApiModelProperty(value = "Blog Comment email")
   @NotEmpty
   @Email
   private String email;
 
+  @ApiModelProperty(name = "Blog Comment body")
   @NotEmpty
   @Size(min = 12, message = "Title must have 12 characters")
   private String body;
--- src/main/java/com/ratz/blog/DTO/PostDTO.java
+++ src/main/java/com/ratz/blog/DTO/PostDTO.java
@@ -1,27 +1,35 @@
 package com.ratz.blog.DTO;
 
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import java.util.Set;
 
+@ApiModel(value = "Post model information")
 @Data
 public class PostDTO {
 
+  @ApiModelProperty(value = "Blog Post ID")
   private Long id;
 
+  @ApiModelProperty(value = "Blog Post Title")
   @NotEmpty
   @Size(min = 2, message = "Title must have 2 characters")
   private String title;
 
+  @ApiModelProperty(value = "Blog Post description")
   @NotEmpty
   @Size(min = 2, message = "Title must have 12 characters")
   private String description;
 
+  @ApiModelProperty(value = "Blog Post content")
   @NotEmpty
   private String content;
 
+  @ApiModelProperty(value = "Blog Post Comments")
   private Set<CommentDTO> comments;
 }
